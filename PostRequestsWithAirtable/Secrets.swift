1f4b17bbeccf4d10162eb8aab429de11f4553e1b
Tiffany Obi <tiffanyobi123@gmail.com>
2019-12-27 15:59:52 -0500

    added api keys

---
+++ PostRequestsWithAirtable/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  PostRequestsWithAirtable
+//
+//  Created by Tiffany Obi on 12/27/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+struct Secrets {
+    static let AirtableAPIKey = "key6hGUmFKd6U1ugw"
+    static let AirtableProject = "appxmgJGUrvYsM5DT"
+}
